26ffb2adea9f9d90c5596b41604df1a3940a8699
jgmoon <6813821@tukorea.ac.kr>
2022-12-22 15:14:54 +0900

    change parameter name

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function teraToGiga(x) {
-    console.log(x + 'TB는');
-    console.log(x * 1024 + 'GB 입니다.');
+function teraToGiga(volume) {
+    console.log(volume + 'TB는');
+    console.log(volume * 1024 + 'GB 입니다.');
   };
   
-  function teraToMega(x) {
-    console.log(x + 'TB는');
-    console.log(x * 1024 * 1024 + 'MB 입니다.');
+  function teraToMega(volume) {
+    console.log(volume + 'TB는');
+    console.log(volume * 1024 * 1024 + 'MB 입니다.');
   };
   
   // TB -> GB 테스트
