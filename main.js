89c77c8081c46e4a7a4872e0f2aa8c4fe137a44c
jgmoon <6813821@tukorea.ac.kr>
2022-12-22 16:01:59 +0900

    add interestCalculator function

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function bmiCalculator(name, weight, heigth) {
-    console.log(name + '님의 체질량지수는 ' + weight/(heigth*heigth/10000) + '입니다.')
+function interestCalculator(amount, term, rate) {
+    let interest = amount * term * rate / 100;
+    return interest;
   };
   
+  // 조건 입력 테스트
+  let myMoney = 3650000; // 맡긴 금액 (원)
+  let saveTerm = 1; // 맡기는 기간 (년)
+  let interestRate = 4; // 이자율 (%)
   
-  // 테스트 코드
-  bmiCalculator('홀쭉이', 43.52, 160);
-  bmiCalculator('코린이', 61.25, 175);
-  bmiCalculator('통통이', 77.76, 180);
+  // 수령액 계산 테스트
+  let myInterest = interestCalculator(myMoney, saveTerm, interestRate);
+  let totalMoney = myMoney + myInterest;
+  
+  // 출력 테스트
+  console.log('맡긴 금액은 ' + myMoney + '원 입니다.');
+  console.log('이자는 ' + myInterest + '원 입니다.');
+  console.log('최종 받을 금액은 ' + totalMoney + '원 입니다.');
