620d0348c8184ae96a10a799adfef08b14de6a24
jgmoon <6813821@tukorea.ac.kr>
2022-12-22 15:13:03 +0900

    add teraToGiga and teraToMega function

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-function printSquare(x) {
-    console.log(x + ' * ' + x);
-    console.log('= ' + x * x);
+function teraToGiga(x) {
+    console.log(x + 'TB는');
+    console.log(x * 1024 + 'GB 입니다.');
   };
   
-  printSquare(2);
-  printSquare(5);
-  printSquare(100);
+  function teraToMega(x) {
+    console.log(x + 'TB는');
+    console.log(x * 1024 * 1024 + 'MB 입니다.');
+  };
+  
+  // TB -> GB 테스트
+  teraToGiga(2);
+  // TB -> MB 테스트
+  teraToMega(2);
+  
+  // 결과
+  // 2TB는
+  // 2048GB 입니다.
+  // 2TB는
+  // 2097152MB 입니다.
