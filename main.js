8a380f7708b4cf92f5025b60e1828f17bdf0f738
jgmoon <6813821@tukorea.ac.kr>
2022-12-27 16:22:21 +0900

    add checkHeight function

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-function orderSetMenu(sandwich, drink = '스프라이트'){
-  console.log(`주문하신 ${sandwich}, ${drink} 세트메뉴 나왔습니다!`);
-};
+function checkHeight(height) {
+	if (height >= 140) {
+	  console.log('탑승이 가능합니다.');
+	} else {
+	  console.log('탑승이 불가능합니다.');
+	}
+}
 
 // 테스트 코드
-orderSetMenu('코드웨잇 클럽');
-orderSetMenu('터키베이컨 아보카도', '코카콜라');
-orderSetMenu('코드웨잇 멜트');
-orderSetMenu('이탈리안 비엠티', '닥터페퍼');
-orderSetMenu('에그마요', '환타 오렌지');
+checkHeight(140);
+checkHeight(135);
+checkHeight(170);
