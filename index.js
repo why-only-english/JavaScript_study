fac5dcf92c439a6cacd75c150bff1064c0d541dc
jgmoon <6813821@tukorea.ac.kr>
2022-12-27 11:53:15 +0900

    null vs undefined

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// 형변환(Type conversion)
+let x;
+console.log(x);  // undefined
+let y = x;
+x = null;
+console.log(y);  // undefined
+console.log(x);  // null
+x = y;
+console.log(x);  // undefined
 
-// String, Number, Boolean
-console.log(String(10) + String(5));
-console.log(Number('10') + Number('5'));
-console.log(Number('10' + '5'));
+let a;
+let b = null;
 
-console.log(typeof('4'+3)); // string
-console.log(typeof(3-true)); // number
-console.log(typeof(Boolean(5)*true)); //number
-console.log(typeof(true<false)); //false
-
-console.log('5'/true!='5'); // false
-
-// 일치 동등 비교
-console.log(2 === '2') // false
-console.log(2 == '2') // true
-
-// 불일치 부등 비교
-console.log(2 !== '2') // true
-console.log(2 != '2') // false
+console.log(a === b);          // false
+console.log(a == b);           // true
+console.log(a == null);        // true
+console.log(b === undefined);  // true
