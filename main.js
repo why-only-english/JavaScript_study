df5dcde6eeb83a46677f5d7a509c75ef00fbdbba
jgmoon <6813821@tukorea.ac.kr>
2022-12-27 13:55:39 +0900

    add expressMultiplication function

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-function calcWage(name, time, wage) {
-  let total = time * wage;
+function expressMultiplication(num1, num2) {
+  result = num1 * num2
+  
+  // console.log(num1 + ' * ' + num2 + ' = ' + result);  
+  console.log(`${num1} * ${num2} = ${result}`);
+};
 
-  console.log(`${name}님의 근무 시간은 총 ${time}시간이며, 최종 급여는 ${total}원 입니다.`); 
-}
 
-calcWage('김윤식', 208, 11340);
-calcWage('성규재', 175, 12160);
-calcWage('손태웅', 161, 13070);
-calcWage('허우선', 222, 10980);
+
+expressMultiplication(3, 4);
+expressMultiplication(3, 2);
+expressMultiplication(7, 5);
+expressMultiplication(8, 9);
+expressMultiplication(5, 5);
+expressMultiplication(9, 9);
