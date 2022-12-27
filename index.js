88078d8270031507fe04925acbf1705f0b774bcb
jgmoon <6813821@tukorea.ac.kr>
2022-12-27 10:55:38 +0900

    Type conversion

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// typeof 연산자
-console.log(typeof typeof 8);
+// 형변환(Type conversion)
 
-// typeof 연산자는 오른쪽에 있는 값의 자료형을 '문자열'로 반환하는 연산자 따라서 string 출력
+// String, Number, Boolean
+console.log(String(10) + String(5));
+console.log(Number('10') + Number('5'));
+console.log(Number('10' + '5'));
+
+console.log(typeof('4'+3)); // string
+console.log(typeof(3-true)); // number
+console.log(typeof(Boolean(5)*true)); //number
+console.log(typeof(true<false)); //false
+
+console.log('5'/true!='5'); // false
+
+// 일치 동등 비교
+console.log(2 === '2') // false
+console.log(2 == '2') // true
+
+// 불일치 부등 비교
+console.log(2 !== '2') // true
+console.log(2 != '2') // false
