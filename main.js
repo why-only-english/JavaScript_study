adf5a041c3291a0d5cbc0ba94a53838767d4909f
jgmoon <6813821@tukorea.ac.kr>
2022-12-27 14:50:14 +0900

    add orderSetMenu function

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-function expressMultiplication(num1, num2) {
-  result = num1 * num2
-  
-  // console.log(num1 + ' * ' + num2 + ' = ' + result);  
-  console.log(`${num1} * ${num2} = ${result}`);
+function orderSetMenu(sandwich, drink = '스프라이트'){
+  console.log(`주문하신 ${sandwich}, ${drink} 세트메뉴 나왔습니다!`);
 };
 
-
-
-expressMultiplication(3, 4);
-expressMultiplication(3, 2);
-expressMultiplication(7, 5);
-expressMultiplication(8, 9);
-expressMultiplication(5, 5);
-expressMultiplication(9, 9);
+// 테스트 코드
+orderSetMenu('코드웨잇 클럽');
+orderSetMenu('터키베이컨 아보카도', '코카콜라');
+orderSetMenu('코드웨잇 멜트');
+orderSetMenu('이탈리안 비엠티', '닥터페퍼');
+orderSetMenu('에그마요', '환타 오렌지');
