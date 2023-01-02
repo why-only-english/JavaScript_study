ecfa6faeec1fc11e17529dcec7ae22db21450b97
jgmoon <6813821@tukorea.ac.kr>
2023-01-02 14:35:06 +0900

    Find the divisor and the number of divisor

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-function printTriangle(height) {
-	let count='';
-  for(i=1; i<=height; i++) {
-    count +='*';
-    console.log(count);
+const N = 180;
+
+let i = 1;
+let count = 0;
+
+while(i <= N) {
+  if(N % i === 0) {
+    console.log(i);
+    count++;
   }
+  i++;
 }
 
-// 테스트 코드
-console.log('높이: 1');
-printTriangle(1);
-
-console.log('높이: 3');
-printTriangle(3);
-
-console.log('높이: 5');
-printTriangle(5);
+console.log(`${N}의 약수는 총 ${count}개 입니다.`)
