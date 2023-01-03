58b9dec85057fae3bde1075feed30e3f2fd589aa
종근 <114092152+why-only-english@users.noreply.github.com>
2023-01-03 16:00:25 +0900

    Update index1.js

--- object/index1.js
+++ object/index1.js
@@ -1,39 +1,13 @@
-// 객체 (object)
-let codeit = {
-    name: '코드잇',
-    bornyear: 2017,
-    isVeryNice: true,
-    worstCourse: null,
-    bestcourse: {
-        title: '자바스크립트 프로그래밍 기초',
-        langauge: 'JavaScript'
-    }
-};
+// 배열 (Array)
+let courseRanking = [
+    '자바스크립트 프로그래밍 기초',
+    'Git으로 배우는 버전 관리',
+    '컴퓨터 개론',
+    '파이썬 프로그래밍 기초'
+];
 
-console.log(codeit.name);
-codeit.name = 'codeit';
-console.log(codeit.name);
+// index == PropertyName
+console.log(courseRanking[0]);
 
-console.log(codeit.name !== undefined);
-
-// in 연산자
-// 안전, if 문의 조건문에도 활용 가능
-console.log('name' in codeit);  
-
-for (let key in codeit) {
-    console.log(key);
-    console.log(codeit[key]);
-}
-
-let myAccount = {
-    'KBank': 1116000,
-    'JBank': 1005000,
-    'CBank': 618000,
-   };
-   
-   let b=5;
-   for (let bank in myAccount) {
-     b += myAccount[bank]
-   }
-   
-   console.log(b);
+console.log(courseRanking[length + 1]);
+console.log(courseRanking['length']);
