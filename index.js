54a9cf9efcd7d239e5d00cd3a39050fde062008f
jgmoon <6813821@tukorea.ac.kr>
2023-01-03 09:40:08 +0900

    syncronized

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-let x;
-console.log(x);  // undefined
-let y = x;
-x = null;
-console.log(y);  // undefined
-console.log(x);  // null
-x = y;
-console.log(x);  // undefined
-
-let a;
-let b = null;
-
-console.log(a === b);          // false
-console.log(a == b);           // true
-console.log(a == null);        // true
-console.log(b === undefined);  // true
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 !== 0) {
+      continue;
+    }
+    console.log(i);
+    i++;
+  }
