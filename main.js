08658a669350dbf6f2b579e51faafaf731c400c2
jgmoon <6813821@tukorea.ac.kr>
2023-01-03 14:43:39 +0900

    Create workDayCalc function

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-let hyesoonScore = {
-	'데이터 모델링의 이해': 10,
-	'데이터 모델과 성능': 8,
-	'SQL 기본': 22,
-	'SQL 활용': 18,
-	'SQL 최적화 기본 원리': 20,
-};
+let today = new Date(2112, 8, 24);
+let jaeSangStart = new Date(2109, 7, 1);
 
-let minsoonScore = {
-	'데이터 모델링의 이해': 14,
-	'데이터 모델과 성능': 8,
-	'SQL 기본': 12,
-	'SQL 활용': 4,
-	'SQL 최적화 기본 원리': 16,
-};
-
-function passChecker(scoreObject) {
-	// 여기에 코드를 작성하세요
-  let sum = 0;
-  
-  for (let key in scoreObject) {
-    sum += scoreObject[key];
-  }
-  
-  if (sum >= 60) {
-      console.log('축하합니다! 합격입니다!');
-    }
-  else {
-      console.log('아쉽지만 불합격입니다..');
-    }
+function workDayCalc(startDate) {
+	let diff = today.getTime() - jaeSangStart.getTime();
+	console.log('오늘은 입사한 지 ' + (diff / 1000 / 60 / 60 / 24 + 1) + '일째 되는 날 입니다.')
 }
 
-passChecker(hyesoonScore);
-passChecker(minsoonScore);
+workDayCalc(jaeSangStart);
