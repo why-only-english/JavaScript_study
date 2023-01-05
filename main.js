d604e1a9a4ef5d90cb83f3afba1f6a3cc5769851
jgmoon <6813821@tukorea.ac.kr>
2023-01-05 09:44:57 +0900

    Create interestCalculator function

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-// 투표 결과 리스트
-let votes = [
-  '이재식', '이재식', '이규하', '이규하', '이규하',
-  '이재식', '이재식', '이규하', '이규하', '이재식',
-  '이규하', '이규하', '이규하', '이규하', '이재식',
-  '이재식', '이규하', '이재식', '이재식', '이재식',
-  '이재식', '이재식', '이규하', '이규하', '이규하',
-  '이규하', '이규하', '이재식', '이규하', '이규하',
-  '이규하', '이규하', '이재식', '이규하', '이규하',
-  '이규하', '이재식', '이재식', '이재식', '이규하',
-];
-
-// 후보별 득표수 객체
-let voteCounter = {};
-
-// votes 배열을 이용해서 voteCounter 객체를 정리하기
-for (let name of votes) {
-   if (name in voteCounter) {
-    voteCounter[name] += 1;
-  } else {
-    voteCounter[name] = 1;
-  }
+// 여기에 코드를 작성하세요
+function interestCalculator(rate, payment, term) {
+  let interest = payment * term * (term + 1) / 2 * rate / 12;
+  // let interest = +(payment * term * (term + 1) / 2 * rate / 12).toFixed(0);
+  console.log(Math.round(interest));
+  // console.log(interest.toFixed(0));
 }
 
-// 후보별 득표수 출력
-console.log(voteCounter);
+
+// 이율이 4.3%일 때 매월 80만원씩 24개월 납입할 경우
+interestCalculator(0.043, 800000, 24);
+
+// 이율이 4.3%일 때 매월 60만원씩 24개월 납입할 경우
+interestCalculator(0.043, 600000, 24);
