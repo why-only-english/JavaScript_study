bb4e37f9c3a1e7267d32ffb93d1207e10c6db788
jgmoon <6813821@tukorea.ac.kr>
2023-01-10 11:59:51 +0900

    Arrow Function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function introduce(name = '홍길동', birth = 1443) {
-    console.log(`안녕하세요 저는 ${name}입니다.`);
-    console.log(`${birth}년에 태어났습니다.`);
-  }
-  
-  introduce('장동건');          // 장동건, 1443
-  introduce('Joy', null);      // Joy, null
-  introduce(undefined, 2090);  // 홍길동, 2090
+// Arrow Function
+const getTwice = number => number * 2;
+
+console.log(getTwice(3));
+
+const sum = (a, b) => {
+    const result = a + b;
+    return result;
+};
+
+const getCodeit = () => ({name: 'Codeit',});
+
+console.log(sum(10, 5));
+console.log(getCodeit());
