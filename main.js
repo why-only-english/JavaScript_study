d6d360fb8dfd73aa5de8d12862630b84600132e3
jgmoon <6813821@tukorea.ac.kr>
2023-01-10 15:27:23 +0900

    Practice Spread statement

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-function ignoreFirst(first, ...others) {
-  for(const a of others) {
-     console.log(a);  
+const snacks = ['원카칩', '꿀버터칩', '헛스윙칩', '태양칩', '야채시간'];
+const drinks = ['사이다', '콜라', '우유', '물', '커피', '레몬에이드'];
+
+function printArguments(...args) {
+  for (const arg of args) {
+    console.log(arg);
   }
 }
 
-// function ignoreFirst(...rest){
-//   for(let i=1; i<rest.length; i++){
-//     console.log(rest[i]);
-//   }
-// }
+// 1. Spread 구문을 활용해서 sancks와 drinks 배열을 각각 mySnacks와 myDrinks 변수에 복사해 주세요
+const mySnacks = [...snacks];
+const myDrinks = [...drinks];
 
-ignoreFirst('1세대', '2세대', '3세대');
-ignoreFirst('곰팡이', '강아지', '고양이');
-ignoreFirst(20, 9, 18, 19, 30, 34, 40);
+mySnacks.splice(2, 3);
+myDrinks.splice(1);
+
+// 2. Spread 구문을 활용해서 mySnacks와 myDrinks 순서로 두 배열을 합쳐서 myChoice 변수에 할당해 주세요
+const myChoice = [...mySnacks, ...myDrinks];
+
+// 3. Spread 구문을 활용해서 myChoice의 각 요소들을 printArguments 함수의 아규먼트로 전달해 주세요
+printArguments(...myChoice);
