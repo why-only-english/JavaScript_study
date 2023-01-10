bec3b9eb3e528dba735458803efc7cd5b59d423d
jgmoon <6813821@tukorea.ac.kr>
2023-01-10 10:43:57 +0900

    parameter and argument

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-for (let i = 1; i <= 50; i++) {
-    if (i % 2 !== 0) {
-      continue;
-    }
-    console.log(i);
-    i++;
+function introduce(name = '홍길동', birth = 1443) {
+    console.log(`안녕하세요 저는 ${name}입니다.`);
+    console.log(`${birth}년에 태어났습니다.`);
   }
+  
+  introduce('장동건');          // 장동건, 1443
+  introduce('Joy', null);      // Joy, null
+  introduce(undefined, 2090);  // 홍길동, 2090
