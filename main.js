69b55645056e072cc977be44d3ffc421cfe2c499
jgmoon <6813821@tukorea.ac.kr>
2023-01-11 11:14:52 +0900

    Destructuring

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-const snacks = ['원카칩', '꿀버터칩', '헛스윙칩', '태양칩', '야채시간'];
-const drinks = ['사이다', '콜라', '우유', '물', '커피', '레몬에이드'];
+// 1. Destructuring 문법을 활용해서 numbers 배열의 각 요소를 one, two, three라는 변수에 할당해보세요
+const numbers = [1, 2, 3];
 
-function printArguments(...args) {
-  for (const arg of args) {
-    console.log(arg);
-  }
-}
+const [one, two, three] = numbers;
 
-// 1. Spread 구문을 활용해서 sancks와 drinks 배열을 각각 mySnacks와 myDrinks 변수에 복사해 주세요
-const mySnacks = [...snacks];
-const myDrinks = [...drinks];
 
-mySnacks.splice(2, 3);
-myDrinks.splice(1);
+// 2. Destructuring 문법을 활용해서 TV는 livingRoom, 나머지 요소들(배열)은 kitchen 변수에 할당해 주세요
+const products = ['TV', '식탁', '냉장고', '전기밥솥', '전자레인지', '오븐', '식기세척기'];
 
-// 2. Spread 구문을 활용해서 mySnacks와 myDrinks 순서로 두 배열을 합쳐서 myChoice 변수에 할당해 주세요
-const myChoice = [...mySnacks, ...myDrinks];
+const [livingRoom, ...kitchen] = products;
 
-// 3. Spread 구문을 활용해서 myChoice의 각 요소들을 printArguments 함수의 아규먼트로 전달해 주세요
-printArguments(...myChoice);
+
+// 3. Destructuring 문법을 활용해서 두 변수의 값을 서로 바꿔주세요
+let firstName = 'Kang';
+let lastName = 'Young';
+
+[firstName, lastName] = [lastName, firstName];
+
+// 테스트 코드
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(livingRoom);
+console.log(kitchen[1]);
+console.log(firstName);
+console.log(lastName);
