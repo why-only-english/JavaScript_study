698fb8952e53087820a4bc4cd74870b6ea8d0967
jgmoon <6813821@tukorea.ac.kr>
2023-01-18 13:36:47 +0900

    polimorphism practice

--- main.js
+++ main.js
@@ -1,25 +1,95 @@
-// function makeCar(color, speed) {
-//   const car = {
-//     color,
-//     speed,
-//     run() {
-//       console.log(`Runs at ${this.speed}`);
-//     },
-//   };
-//   return car;
-// }
-
-class Car {
-  constructor(color, speed) {
-    this.color = color;
-    this.speed = speed;
+class BankAccount {
+  constructor(name, money) {
+    this.holder = name;
+    this.balance = money;
   }
-  
-  run() {
-    console.log(`Runs at ${this.speed}`);
+
+  get balance() {
+    return this._balance;
+  }
+
+  set balance(money) {
+    if (money >= 0) {
+      this._balance = money;
+    } else {
+      console.log('Not valid');
+    }
+  }
+
+  deposit(money) {
+    this.balance += money;
+  }
+
+  withdraw(money) {
+    if (this.balance - money < 0) {
+      console.log('Insufficient balance');
+    } else {
+      this.balance -= money;
+    }
+  }
+
+  transfer(money, anotherAccount) {
+    const account = anotherAccount;
+    if (this.balance - money < 0) {
+      console.log('Insufficient balance');
+    } else {
+      this.balance -= money;
+      account.balance += money;
+    }
   }
 }
 
-const car1 = new makeCar('blue', '100km/h');
+class SavingsAccount extends BankAccount {
+  constructor(name, money) {
+    super(name, money);
+    this.years = 0;
+  }
 
-car1.run();
+  addInterest(rate) {
+    this.balance *= (1 + (rate * this.years));
+  }
+  
+  transfer(money, anotherAccount) {
+    super.transfer(money, anotherAccount);
+    this.balance -= money * 0.005;
+  }
+}
+
+class DonationAccount extends BankAccount {
+  constructor(name, money, rate) {
+    super(name, money);
+    this.rate = rate;
+  }
+
+  donate(rate) {
+    this.balance *= (1 - this.rate);
+  }
+
+  transfer(money, anotherAccount) {
+    super.transfer(money, anotherAccount);
+    this.balance -= money * 0.002;
+  }
+}
+
+const ba1 = new BankAccount('Tom', 20000000);
+const sa1 = new SavingsAccount('Jerry', 10000000);
+const da1 = new DonationAccount('Kate', 30000000);
+const sa2 = new SavingsAccount('Alice', 9000000);
+
+const accountForVacation = new BankAccount('Vacation', 0);
+
+accounts = [ba1, sa1, da1, sa2]
+
+for (account of accounts) {
+  account.transfer(80000, accountForVacation);
+}
+// ba1.transfer(800000, accountForVacation);
+// sa1.transfer(800000, accountForVacation);
+// da1.transfer(800000, accountForVacation);
+// sa2.transfer(800000, accountForVacation);
+
+console.log(ba1.balance);
+console.log(sa1.balance);
+console.log(da1.balance);
+console.log(sa2.balance);
+console.log(accountForVacation.balance);
