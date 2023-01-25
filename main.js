d73554f46b1c676806dbd4e2ef60446d28ce0b86
jgmoon <6813821@tukorea.ac.kr>
2023-01-25 14:10:26 +0900

    Add newmember

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-// fetch('https://learn.codeit.kr/api/topics')
-//     .then((response) => response.text())
-//     .then((result) => { console.log(result) })
+// 새 직원 정보를 추가한 후에, 전체 직원 정보를 조회하는 코드
+// 새 직원 정보는 원하는 대로 작성하세요
+const newMember = {
+  name: 'jg',
+  email: 'jg@codeitmall.kr',
+	department: 'engineering',
+};
 
-// '초급'에 해당하는 토픽들만 불러오기 
-fetch('https://learn.codeit.kr/api/topics')
-  .then((response) => response.text())
-  .then((result) => {
-    const topics = JSON.parse(result);
-    const beginnerLevelTopics = topics.filter((topic) => topic.difficulty === '초급');
-    console.log(beginnerLevelTopics);
+
+fetch('https://learn.codeit.kr/api/members', {
+  method: 'POST',
+  body: JSON.stringify(newMember),  // 직렬화
+})
+  .then(() => {
+    fetch('https://learn.codeit.kr/api/members')
+      .then((response) => response.text())
+      .then((result) => {
+        const members = JSON.parse(result);  // 역직렬화 
+        // 가장 마지막에 존재하는(가장 최근에 추가된) 직원 정보
+        console.log(members[members.length - 1]);  
+      });
   });
