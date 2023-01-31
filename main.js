a3d1f623b1861fa59dc03ea423ea9611586661d9
jgmoon <6813821@tukorea.ac.kr>
2023-01-31 14:29:34 +0900

    try, catch, finally quiz

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-// fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((response) => response.json())
-//   .then((users) => {
-//     const lastUser = users[users.length - 1];
-//     return lastUser.id;
-//   })
-//   .then((id) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`))
-//   .then((response) => response.json())
-//   .then((posts) => {
-//     const lastPost = posts[posts.length - 1];
-//     console.log(lastPost);
-//   });
-
-// async/await 구문으로 실행 코드 작성(비동기 실행)
-async function getTheLastPostOfTheLastUser() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
-  const lastUser = users[users.length - 1];
-  const { id } = lastUser;
-  const postsJSON = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
-  const posts = await postsJSON.json();
-  const lastPost = posts[posts.length - 1];
-  return lastPost;
+async function showQuiz() {
+  try {
+    const response = await fetch('https://learn.codeit.kr/api/quiz');
+    const test = await response.json();
+    const yourAnswer = prompt(test.quiz);
+    if (yourAnswer.toLowerCase() === test.answer) {
+      alert(`Good Job, ${test.explanation} => Let\'s learn more with Codeit!`);
+    } else {
+      throw new Error('wrong');
+    }
+  } catch (error) {
+    if (error.message === 'wrong') {
+      alert('You need to learn JavaScript with Codeit!');
+    } else {
+      alert('Error');
+    }
+  } finally {
+    window.open('https://codeit.kr', '_blank');
+  }
 }
 
-getTheLastPostOfTheLastUser().then((lastPost) => {
-  console.log(lastPost);
-});
+showQuiz();
